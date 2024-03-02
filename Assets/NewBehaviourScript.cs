4803690ae4177bd8d3b2ddbdf09ba1ec0c0ddb30
besteiyidogan <161970081+besteiyidogan@users.noreply.github.com>
2024-03-03 01:01:22 +0300

    My-project

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+
+    public int counter = 0;
+    public int speed = 10;
+    public GameObject startText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start");
+        startText.GetComponent<TextMeshPro>().text = "Start";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+}
